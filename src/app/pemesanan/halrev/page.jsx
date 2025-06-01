97c2e157fab0f3ae4a4ec770349936c2b184c983
wendwin <wendwin12@gmail.com>
2025-06-01 23:25:23 +0700

    solve conflict

--- src/app/pemesanan/halrev/page.jsx
+++ src/app/pemesanan/halrev/page.jsx
@@ -5,7 +5,6 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import ReviewItem from '../../../../components/harlev/ReviewItem';
 import StepIndicator from '../../../../components/harlev/StepIndicator';
 import BackToFormLink from '../../../../components/harlev/BackToFormLink';
 import PaymentModal from '../../../../components/harlev/PaymentModal';
