8816b3ac9cad38c08133b9eb34dffff30fcf4443
devianadyah <devianadyaaah@gmail.com>
2025-06-01 17:57:37 +0700

    melakukan perubahan artikel

--- components/hero/ArtikelCard.jsx
+++ components/hero/ArtikelCard.jsx
@@ -5,20 +5,30 @@ export default function ArticleCard({ article }) {
   const router = useRouter();
 
   return (
-    <div
-      className="bg-white border border-gray-200 rounded-xl shadow-sm hover:shadow-md transition-transform duration-300 hover:-translate-y-1 p-4 cursor-pointer"
-      onClick={() => router.push(`/artikel/${article.id}`)}
-    >
-      <div className="w-full h-48 relative rounded-md overflow-hidden">
-        <img
-          src={`http://127.0.0.1:8000/storage/gambar/${article.gambar}`}
-          alt={article.gambar}
-          className="object-cover w-full h-full"
-        />
-      </div>
-      <p className="mt-4 text-base font-semibold text-gray-700">
-        {article.judul}
-      </p>
-    </div>
+      <div className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden flex flex-col cursor-pointer" 
+      onClick={() => router.push(`/artikel/${article.id}`)}>
+                    <img
+                      src={`http://127.0.0.1:8000/storage/gambar/${article.gambar}`}
+                      alt={article.gambar}
+                      width={500}
+                      height={300}
+                      className="w-full h-52 object-cover"
+                    />
+                    <div className="p-6 flex-1 flex flex-col justify-between">
+                      <div>
+                        <h3 className="text-lg font-semibold text-gray-800 mb-2">
+                          {article.judul}
+                        </h3>
+                          {article.isi_konten.length > 100
+                          ? article.isi_konten.slice(0, 100) + "..."
+                          : article.isi_konten}
+                      </div>
+                      <div>
+                        <button className="text-blue-600 hover:text-blue-800 text-sm font-medium transition">
+                          Baca Selengkapnya →
+                        </button>
+                      </div>
+                    </div>
+                  </div>
   );
 }
