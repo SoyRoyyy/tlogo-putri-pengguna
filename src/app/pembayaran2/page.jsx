b8e913746615cda0dd1b2a8c730c515a4fd0c91b
SoyRoyyy <fikrymaulanaaa06@gmail.com>
2025-06-16 19:45:49 +0700

    solved conflict

--- src/app/pembayaran2/page.jsx
+++ src/app/pembayaran2/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react"; // <-- 1. Tambahkan import Suspense
 import PembayaranSisaTagihan from "../../../components/paymentwo/sisaTagihan";
 import Header from "../../../components/hero/HeaderPayment2";
 import Footer from "../../../components/hero/Footer";
@@ -9,9 +10,11 @@ export default function SisaTagihanPage() {
     <main className="bg-[#F3F3F7] text-gray-800 font-sans min-h-screen flex flex-col">
       <Header />
 
-      {/* Tambahkan div wrapper untuk konten agar bisa flex-grow */}
       <div className="flex-grow">
-        <PembayaranSisaTagihan />
+        {/* 2. Bungkus komponen yang bermasalah dengan Suspense */}
+        <Suspense fallback={<div className="text-center p-10">Memuat data tagihan...</div>}>
+          <PembayaranSisaTagihan />
+        </Suspense>
       </div>
 
       <Footer />
