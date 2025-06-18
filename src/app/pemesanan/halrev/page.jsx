289ce269b07ef1d2b77ea94597d6634015313aa6
SoyRoyyy <fikrymaulanaaa06@gmail.com>
2025-06-18 08:16:18 +0700

    fix halrev errro merge

--- src/app/pemesanan/halrev/page.jsx
+++ src/app/pemesanan/halrev/page.jsx
@@ -1,3 +1,205 @@
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+// import StepIndicator from "@/components/harlev/StepIndicator";
+// import BackToFormLink from "@/components/harlev/BackToFormLink";
+// import PaymentModal from "@/components/harlev/PaymentModal";
+// import PaketForms from "@/components/hero/PaketForms";
+// import ReviewSummary from "@/components/ReviewSummary";
+// import ReviewLoading from "@/components/ReviewLoading";
+// import { createBooking, cancelBooking, getBookingByOrderId } from "../../lib/api";
+
+// export default function ReviewPage() {
+//   const [data, setData] = useState(null);
+//   const [token, setToken] = useState(null);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [paymentType, setPaymentType] = useState(null);
+//   const [isSnapLoading, setIsSnapLoading] = useState(false);
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     if (typeof window === "undefined") return;
+
+//     const urlToken = new URLSearchParams(window.location.search).get("token");
+//     const savedToken = localStorage.getItem("token");
+//     const finalToken = urlToken || savedToken;
+
+//     if (finalToken) {
+//       localStorage.setItem("token", finalToken);
+//       setToken(finalToken);
+//     }
+
+//     const stored = localStorage.getItem("formPemesanan");
+//     if (stored) setData(JSON.parse(stored));
+//   }, []);
+
+//   useEffect(() => {
+//     if (isModalOpen && typeof window !== "undefined") {
+//       setPaymentType(localStorage.getItem("payment_type"));
+//     }
+//   }, [isModalOpen]);
+
+//   const handleBayar = async (mode) => {
+//     if (typeof window === "undefined") return;
+
+//     localStorage.setItem("payment_type", mode);
+//     setIsModalOpen(false);
+
+//     const stored = localStorage.getItem("formPemesanan");
+//     if (!stored) return alert("Data pembayaran tidak ditemukan");
+
+//     const parsed = JSON.parse(stored);
+//     delete parsed.paket;
+//     parsed.payment_type = mode;
+//     parsed.qty = Number(parsed.qty);
+
+//     const existingOrderId = localStorage.getItem("order_id");
+//     const existingSnapToken = localStorage.getItem("snap_token");
+
+//     // const normalizeOrderId = (orderId) => {
+//     //       const parts = orderId.split("-");
+//     //       if (parts.length >= 3) {
+//     //         return parts.slice(0, 3).join("-");
+//     //       }
+//     //       return orderId;
+//     // };
+
+//     // const handleSnapSuccess = async (result) => {
+//     //   localStorage.clear(); 
+//     //   const normalizedOrderId = normalizeOrderId(result.order_id);
+
+
+//     //   try {
+//     //     const booking = await getBookingByOrderId(normalizedOrderId);
+//     //     localStorage.setItem("pending_booking", JSON.stringify(booking));
+        
+//     //     if ((booking.booking_status === 'pending' && booking.payment_status === 'unpaid') || booking.booking_status === 'settlement' && booking.payment_status === 'unpaid'){
+//     //       setTimeout(() => {
+//     //         window.location.href = '/pemesanan/pending';
+//     //       }, 1000);
+//     //     } else {
+//     //       setTimeout(() => {
+//     //         window.location.href = '/pemesanan/success';
+//     //       }, 1000);
+//     //     }
+//     //   } catch (error) {
+//     //        setTimeout(() => {
+//     //       window.location.href = '/pemesanan/pending';
+//     //     }, 1000);
+//     //   }
+//     // };
+
+//   const triggerSnap = (snapToken) => {
+//     if (!window.snap?.pay) return alert("Midtrans belum dimuat");
+
+//       setIsSnapLoading(true);
+//       window.snap.pay(snapToken, {
+//         onSuccess(result) {
+
+//            handleSnapSuccess(result);
+//           console.log('Pembayaran berhasil:', result);
+//           localStorage.clear();
+//           setTimeout(() => window.location.href = '/pemesanan/success', 1000);
+
+//         },
+//         onPending(result) {
+//           console.log("Menunggu pembayaran:", result);
+//         },
+//         onError(result) {
+//           console.log("Terjadi kesalahan:", result);
+//         },
+//         onClose() {
+//           console.log("Popup pembayaran ditutup");
+//           setPaymentType(localStorage.getItem("payment_type"));
+//           setIsSnapLoading(false);
+//         },
+//       });
+//     };
+
+//   if (existingSnapToken && existingOrderId) {
+//     return triggerSnap(existingSnapToken);
+//   }
+
+//   try {
+//     const result = await createBooking(parsed);
+//     localStorage.setItem("payment_type", result.order.payment_type);
+//     localStorage.setItem("snap_token", result.snap_token);
+//     localStorage.setItem("order_id", result.order.order_id);
+
+//     triggerSnap(result.snap_token);
+//   } catch (err) {
+//     console.error(err);
+//     alert("Terjadi kesalahan saat membuat pesanan.");
+//   }
+
+//   const handleCancel = async () => {
+//     const orderId = localStorage.getItem("order_id");
+
+//     try {
+//       if (orderId) await cancelBooking(orderId);
+//       ["payment_type", "snap_token", "order_id", "token"].forEach((key) =>
+//         localStorage.removeItem(key)
+//       );
+
+//       setPaymentType(null);
+//       setIsModalOpen(false);
+//       router.push("/pemesanan/halrev");
+//     } catch (error) {
+//       console.error("Gagal membatalkan pesanan:", error);
+//       alert("Gagal membatalkan pesanan. Silakan coba lagi.");
+//     }
+//   };
+
+//   if (!data) return <ReviewLoading />;
+
+//   const total = data.gross_amount * data.qty;
+
+//   return (
+//     <main className="bg-gray-50 text-gray-900 font-sans min-h-screen w-full">
+//       <PaketForms currentStep={2} />
+
+//       <div className="px-4 sm:px-6 mt-6 mb-2">
+//         <BackToFormLink token={token} />
+//       </div>
+
+//       <section className="px-4 sm:px-6 py-6 w-full mx-auto">
+//         <h2 className="text-lg sm:text-2xl font-semibold text-center text-blue-800 mb-4">
+//           Review Pemesanan Anda
+//         </h2>
+
+//         <div className="block sm:hidden mb-4">
+//           <StepIndicator />
+//         </div>
+
+//         <ReviewSummary
+//           data={data}
+//           total={total}
+//           onBayarClick={() => setIsModalOpen(true)}
+//         />
+//       </section>
+
+//       <PaymentModal
+//         isOpen={isModalOpen}
+//         onClose={() => setIsModalOpen(false)}
+//         onConfirmDP={() => handleBayar("dp")}
+//         onConfirmFull={() => handleBayar("full")}
+//         onCancel={handleCancel}
+//         amount={total}
+//         paymentType={paymentType}
+//       />
+
+//       {isSnapLoading && (
+//         <div className="flex items-center justify-center mt-10">
+//           <span className="text-sm text-gray-600 animate-pulse">
+//             Memuat pembayaran...
+//           </span>
+//         </div>
+//       )}
+//     </main>
+//   );
+// }
+
 
 "use client";
 
@@ -58,37 +260,44 @@ export default function ReviewPage() {
     const existingOrderId = localStorage.getItem("order_id");
     const existingSnapToken = localStorage.getItem("snap_token");
 
-    // const normalizeOrderId = (orderId) => {
-    //       const parts = orderId.split("-");
-    //       if (parts.length >= 3) {
-    //         return parts.slice(0, 3).join("-");
-    //       }
-    //       return orderId;
-    // };
+    const normalizeOrderId = (orderId) => {
+          const parts = orderId.split("-");
+          if (parts.length >= 3) {
+            return parts.slice(0, 3).join("-");
+          }
+          return orderId;
+    };
 
-    // const handleSnapSuccess = async (result) => {
-    //   localStorage.clear(); 
-    //   const normalizedOrderId = normalizeOrderId(result.order_id);
+    const handleSnapSuccess = async (result) => {
+      localStorage.clear(); 
+      const normalizedOrderId = normalizeOrderId(result.order_id);
 
-    //   try {
-    //     const booking = await getBookingByOrderId(normalizedOrderId);
-    //     localStorage.setItem("pending_booking", JSON.stringify(booking));
-        
-    //     if ((booking.booking_status === 'pending' && booking.payment_status === 'unpaid') || booking.booking_status === 'settlement' && booking.payment_status === 'unpaid'){
-    //       setTimeout(() => {
-    //         window.location.href = '/pemesanan/pending';
-    //       }, 1000);
-    //     } else {
-    //       setTimeout(() => {
-    //         window.location.href = '/pemesanan/success';
-    //       }, 1000);
-    //     }
-    //   } catch (error) {
-    //        setTimeout(() => {
-    //       window.location.href = '/pemesanan/pending';
-    //     }, 1000);
-    //   }
-    // };
+      try {
+        const booking = await getBookingByOrderId(normalizedOrderId);
+        localStorage.setItem("pending_booking", JSON.stringify(booking));
+
+        if (
+          (booking.booking_status === "pending" &&
+            booking.payment_status === "unpaid") ||
+          (booking.booking_status === "settlement" &&
+            booking.payment_status === "unpaid")
+        ) {
+          setTimeout(() => {
+            window.location.href = "/pemesanan/pending";
+          }, 1000);
+        } else {
+          // Clear localStorage di sini sebelum redirect ke success
+          localStorage.clear();
+          setTimeout(() => {
+            window.location.href = "/pemesanan/success";
+          }, 1000);
+        }
+      } catch (error) {
+        setTimeout(() => {
+          window.location.href = "/pemesanan/pending";
+        }, 1000);
+      }
+    };
 
     const triggerSnap = (snapToken) => {
       if (!window.snap?.pay) return alert("Midtrans belum dimuat");
@@ -98,9 +307,9 @@ export default function ReviewPage() {
         onSuccess(result) {
 
            handleSnapSuccess(result);
-          console.log('Pembayaran berhasil:', result);
-          localStorage.clear();
-          setTimeout(() => window.location.href = '/pemesanan/success', 1000);
+          // console.log('Pembayaran berhasil:', result);
+          // localStorage.clear();
+          // setTimeout(() => window.location.href = '/pemesanan/success', 1000);
 
         },
         onPending(result) {
